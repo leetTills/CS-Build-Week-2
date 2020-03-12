e32a462a0537fb98b9924c34a13a4f5c8e7deada
Tyler <tylerwesleyquinn@gmail.com>
2020-03-11 23:56:17 -0400

    cleans up code base

--- action_traversal.py
+++ action_traversal.py
@@ -89,12 +89,6 @@ if status['name'][:4] == 'User':
     status = post(end['status'], {})
     print(status)
 if status['name'][:4] != 'User':
-    # print('status ---->', status)
-    # # 55 is wishing well
-    # well = gr.get_path_to_room(data, 55)
-    # print("wishing well ---->", well)
-    # mine_room = gr.get_path_to_room(data, int(
-    #     input('Which room is your mining room? ')))
     balance = get(end['bal'])
     print('balance ---->', balance)
     while True:
